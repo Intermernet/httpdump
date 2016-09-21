7dcd06bffa79176649464a096899714552d6191d
Mike Hughes <intermernet@gmail.com>
2016-09-21 17:40:57 +1000

    Change to string output

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"compress/gzip"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -14,27 +13,11 @@ type server struct{}
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%v\n", r.Header)
 	defer r.Body.Close()
-	for _, enc := range r.Header["Accept-Encoding"] {
-		switch {
-		case enc == "gzip":
-			gz, err := gzip.NewReader(r.Body)
-			if err != nil {
-				log.Printf("%v\n", err)
-			}
-			b, err := ioutil.ReadAll(gz)
-			if err != nil {
-				log.Printf("%v\n", err)
-			}
-			log.Printf("%v\n", b)
-		default:
-			b, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				log.Printf("%v\n", err)
-			}
-			log.Printf("%v\n", b)
-		}
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("%v\n", err)
 	}
-
+	log.Printf("%s\n", b)
 	w.WriteHeader(http.StatusOK)
 }
 
