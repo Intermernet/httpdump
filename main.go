08d0da4421d76173a746df20d8c82ed63765baee
Mike Hughes <intermernet@gmail.com>
2016-09-27 15:57:29 +1000

    Format logging

--- main.go
+++ main.go
@@ -11,13 +11,19 @@ var port = ":8080"
 type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v\n", r.Header)
-	defer r.Body.Close()
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Printf("%v\n", err)
+	log.Printf("Request received from %s\n", r.RemoteAddr)
+	log.Println("Headers:")
+	for k, v := range r.Header {
+		log.Printf("\t%s\t%s\n", k, v)
 	}
-	log.Printf("%s\n", b)
+	if r.ContentLength > 0 {
+		defer r.Body.Close()
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("%v\n", err)
+		}
+		log.Printf("%s\n", b)
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
