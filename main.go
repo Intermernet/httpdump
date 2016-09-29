916f207a3ee4146d745842cb811ee6075a6f6560
Mike Hughes <intermernet@gmail.com>
2016-09-29 13:19:43 +1000

    Add -addr flag and make JSON detection more robust

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,18 +11,22 @@ import (
 	"strings"
 )
 
-var port = ":8080"
+var addr string
+
+func init() {
+	flag.StringVar(&addr, "addr", ":8080", "address to listen on (:8080, localhost:1234 etc.)")
+}
 
 type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request received from %s\n", r.RemoteAddr)
-	log.Println("Headers:")
+	fmt.Println("Headers:")
 	for k, v := range r.Header {
 		fmt.Printf("%s\t%s\n", k, v)
 	}
 	if r.ContentLength > 0 {
-		log.Println("Body:")
+		fmt.Println("Body:")
 		defer r.Body.Close()
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -29,8 +34,17 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		// Parse Content-Types to check if JSON or variant.
+		// See https://www.iana.org/assignments/media-types/media-types.xhtml for full list
+		js := false
+		for _, cts := range r.Header["Content-Type"] {
+			ct := strings.Split(cts, ";")[0]   // Split at ";" to discard charset if defined
+			if strings.HasSuffix(ct, "json") { // All JSON content-types should end in "json"
+				js = true
+			}
+		}
 		switch {
-		case strings.HasSuffix(r.Header["Content-Type"][0], "json"):
+		case js:
 			var out bytes.Buffer
 			if err := json.Indent(&out, b, "", "  "); err != nil {
 				log.Printf("%v\n", err)
@@ -46,8 +60,9 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Printf("Listening on port %s\n", port)
+	flag.Parse()
+	log.Printf("Listening at address %s\n", addr)
 	s := new(server)
 	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
